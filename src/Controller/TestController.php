aeaffbdab2e94e753d82c731898c09fd1e92de92
Lee Cant <leerobertcant@gmail.com>
2018-12-15 03:54:23 +0000

    Setup a few controllers with passing data

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,18 +4,27 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class TestController
+//This controller routes to a Twig file which is part
+//of a templating Engine just like Express.
+class TestController extends Controller
 {
     /**
      * @Route("/");
+     * @Method({"GET"})
      */
     public function GetHtmlPage()
     {
-        return new Response('<html>
+        /*return new Response('<html>
                                 <body>
                                     <h1>Hi!</h1>
                                 </body>
-                            </html>');
+                            </html>');*/
+
+        //Arrays allows data to be passed to the view
+        return $this->render('test/test.html.twig',
+                            array('data' => 'Passed data'));
     }
 }
